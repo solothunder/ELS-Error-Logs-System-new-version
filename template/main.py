966b50de80895b352176132bff366424495f8e66
Solo-Thunder <55307835+solothunder@users.noreply.github.com>
2022-11-29 07:00:50 +0900

    Add files via upload

---
+++ template/main.py
@@ -0,0 +1,8 @@
+from els import els
+els.check_platform("True")
+path = ['./a.txt', './b.txt', './c.txt']
+for pathcount in path:
+    els.check_file_dir(pathcount, "True", "True")
+els.check_ping("google.com", "2", "True", "True")
+els.private_ip("8.8.8.8", 80, "True")
+els.global_ip("True", "True")
